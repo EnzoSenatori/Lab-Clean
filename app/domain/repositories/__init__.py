2feb62c21cf93cdab9082cd1b44e7921eb972767
EnzoSenatori <enzosenatori2@gmail.com>
2026-04-25 17:03:50 -0300

    ES: repositories
    Desenvolvimento de livro_repository.py e reserva_repository.py

---
+++ app/domain/repositories/__init__.py
@@ -0,0 +1,4 @@
+from app.domain.repositories.livro_repository import LivroRepository
+from app.domain.repositories.reserva_repository import ReservaRepository
+
+__all__ = ["LivroRepository", "ReservaRepository"]
